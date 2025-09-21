f5742ede121485b271cf6c5c673805b85202f696
DogGuyMan <escatrgot@gmail.com>
2025-09-21 15:52:07 +0900

    add : 기하 노트

--- Problems/BOJ_2166/main.cpp
+++ Problems/BOJ_2166/main.cpp
@@ -11,45 +11,37 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef tuple<int, int, int> tiii;
 
-struct vector3{
-	ll x;
-	ll y;
-	ll z;
+struct vec3{
+	ll x, y, z;
 };
 
-vector3 Cross(const vector3& a, const vector3& b) {
-	vector3 res;
-	res.x = a.y * b.z - a.z * b.y;
-	res.y = a.z * b.x - a.x * b.z;
-	res.z = a.x * b.y - a.y * b.x;
-	return res;
+inline vec3 Cross(const vec3& a, const vec3& b) {
+	return { (a.y * b.z - a.z * b.y), (a.z * b.x - a.x * b.z), (a.x * b.y - a.y * b.x) };
 }
 
-vector3 Neg(const vector3& v) {
+inline vec3 Neg(const vec3& v) {
 	return {-v.x, -v.y, -v.z};
 }
 
-vector3 Add(const vector3& a, const vector3& b) {
+inline vec3 Add(const vec3& a, const vec3& b) {
 	return {a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
-vector3 Sub(const vector3& a, const vector3& b) {
+inline vec3 Sub(const vec3& a, const vec3& b) {
 	return {a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-ll Det(const vector3& a, const vector3& b)  {
-	ll i = a.y * b.z - a.z * b.y;
-	ll j = a.x * b.z - a.z * b.x;
-	ll k = a.x * b.y - a.y * b.x;
-	return i - j + k;
+inline ll Det(const vec3& a, const vec3& b)  {
+	return (a.y * b.z - a.z * b.y) - (a.x * b.z - a.z * b.x) + (a.x * b.y - a.y * b.x);
 }
 
-deque<vector3> points;
+deque<vec3> points;
 
 void HandleInput(istream &ins)
 {
 	int N; ins >> N;
-	ll xPrev, yPrev; ins >> xPrev >> yPrev;
+	ll xPrev, yPrev; 
+	ins >> xPrev >> yPrev;
 	for(int i = 0; i < N; i++) {
 		ll xCur, yCur; ins >> xCur >> yCur;
 		points.push_back({xCur - xPrev, yCur - yPrev, 0});
