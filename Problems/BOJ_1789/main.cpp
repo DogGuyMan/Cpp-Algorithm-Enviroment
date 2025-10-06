1b20533e160f3fc8f0c39f5cdb58ac52783b436e
DogGuyMan <escatrgot@gmail.com>
2025-10-06 12:41:54 +0900

    update : solve 1789

---
+++ Problems/BOJ_1789/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO                       \
+	ios::sync_with_stdio(false); \
+	cin.tie(nullptr);
+#define FOR(i, a, b) for( int i = (a); i < (b); ++i)
+#define Y first
+#define X second
+#define ALL(v) (v).begin(), (v).end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+ull N;
+void HandleInput(istream &ins)
+{
+	ins >> N;
+	ull sum = 0;
+	int cnt = 0;
+	while(sum + cnt < N) { 
+		++cnt;
+		sum += cnt; 
+	}
+	cout << cnt << '\n';
+}
+
+void HandleQuery(const char *FILE_PATH)
+{
+	fstream fs(FILE_PATH);
+	if (fs.is_open())
+	{
+		HandleInput(fs);
+	}
+}
+
+int main(int argc, const char *args[])
+{
+	if (argc <= 1)
+	{
+		FASTIO;
+		HandleInput(cin);
+	}
+	else
+	{
+		HandleQuery(args[1]);
+	}
+}
